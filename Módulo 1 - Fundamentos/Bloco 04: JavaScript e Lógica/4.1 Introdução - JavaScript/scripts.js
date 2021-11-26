9f535fb382b8d58ce2f5c102dc95661e1996b03d
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-11-26 18:54:35 -0300

    Envio do primeiro exercicio de JS.

---
+++ Módulo 1 - Fundamentos/Bloco 04: JavaScript e Lógica/4.1 Introdução - JavaScript/scripts.js
@@ -0,0 +1,13 @@
+const myName = "Isaac";
+const birthCity = "Betim";
+var birthYear = 1991;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+birthCity = "BH";
+
+console.log(birthYear);
+console.log(birthCity);
