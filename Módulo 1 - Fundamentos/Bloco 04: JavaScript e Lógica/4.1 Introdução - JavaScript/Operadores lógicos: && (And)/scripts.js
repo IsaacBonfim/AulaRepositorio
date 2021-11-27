2cbe66fa6b1d8973191e52ec036f9e3982896895
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-11-27 14:11:04 -0300

    Alteraçao na identaçao dos arquivos de exercicio do bloco 4

--- Módulo 1 - Fundamentos/Bloco 04: JavaScript e Lógica/4.1 Introdução - JavaScript/Operadores lógicos: && (And)/scripts.js
+++ Módulo 1 - Fundamentos/Bloco 04: JavaScript e Lógica/4.1 Introdução - JavaScript/Operadores lógicos: && (And)/scripts.js
@@ -2,19 +2,19 @@ const currentHour = 11;
 let message = '';
 
 if (currentHour >= 22 && currentHour <= 24 ) {
-    message = "Não deveríamos comer nada, é hora de dormir";
+  message = "Não deveríamos comer nada, é hora de dormir";
 } else if (currentHour >= 18 && currentHour < 22) {
-    message = "Rango da noite, vamos jantar :D";
+  message = "Rango da noite, vamos jantar :D";
 } else if (currentHour >= 14 && currentHour < 18) {
-    message = "Vamos fazer um bolo pro café da tarde?";
+  message = "Vamos fazer um bolo pro café da tarde?";
 } else if (currentHour >= 11 && currentHour < 14) {
-    message = "Hora do almoço!!!";
+  message = "Hora do almoço!!!";
 } else if (currentHour >= 4 && currentHour < 11) {
-    message = "Hmmm, cheiro de café recém passado";
+  message = "Hmmm, cheiro de café recém passado";
 } else if (currentHour >= 1 && currentHour < 4) {
-    message = "Ta muito tarde ou muito cerdo, depende do seu ponto de vista.";
+  message = "Ta muito tarde ou muito cerdo, depende do seu ponto de vista.";
 } else {
-    message = "Essa hora não existe.";
+  message = "Essa hora não existe.";
 }
 
 console.log(message);
