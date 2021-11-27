ba0b1e79ea1a3cc877f3b5e90b5364dbe9562e1d
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-11-26 22:25:36 -0300

    Segundo exercicio verificando a tipagem dinamica do JS.

---
+++ Módulo 1 - Fundamentos/Bloco 04: JavaScript e Lógica/4.1 Introdução - JavaScript/Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas/scripts.js
@@ -0,0 +1,23 @@
+let patientId = 50;
+let isEnrolled = true;
+
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+
+patientId = '50';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+/*
+* O objetivo do exercício era verificar o tipo que cada variável assumiriá de acordo com a Informação atribuida a ela.
+*/
