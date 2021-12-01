732e5ac977a242a231d37767d9de4f5b7e5d1c2d
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-01 18:29:52 -0300

    Execicios de fixaçao do dia 4.4 referente a leitores

--- Módulo 1 - Fundamentos/Bloco 04: JavaScript e Lógica/4.4 JavaScript - Objetos e funções/Exercícios de Fixação/Parte I - For-In/Ex1_Turma-Pato-Donald.js
+++ Módulo 1 - Fundamentos/Bloco 04: JavaScript e Lógica/4.4 JavaScript - Objetos e funções/Exercícios de Fixação/Parte I - For-In/Ex1_Turma-Pato-Donald.js
@@ -6,12 +6,12 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-// TODO 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome:
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome:
 
 console.log("Bem-vinda, " + info.personagem + "\n");
 
-// TODO 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, 
-// TODO     imprima o objeto no console:
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, 
+//     imprima o objeto no console:
 
 info['recorrente'] = 'Sim';
 
@@ -21,7 +21,7 @@ for (let key in info) {
 
 }
 
-// TODO 3 - Faça um for/in que mostre todas as chaves do objeto:
+// 3 - Faça um for/in que mostre todas as chaves do objeto:
 
 for (let key in info) {
   
@@ -29,7 +29,7 @@ for (let key in info) {
 
 }
 
-// TODO 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto:
+// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto:
 
 for (let key in info) {
   
@@ -37,13 +37,13 @@ for (let key in info) {
 
 }
 
-// TODO 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e 
-// TODO     os seguintes valores: 
-// TODO      - 'Tio Patinhas', 
-// TODO      - 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 
-// TODO      - 'O último MacPatinhas', 
-// TODO      - 'Sim'. 
-// TODO     Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves:
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e 
+//     os seguintes valores: 
+//       - 'Tio Patinhas', 
+//       - 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 
+//       - 'O último MacPatinhas', 
+//       - 'Sim'. 
+//     Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves:
 
 let info2 = {
   personagem: 'Tio Patinhas',
