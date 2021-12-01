630e15b18ebb55ed95f8c1e8fd272796bf8a6bc2
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-01 15:42:14 -0300

    Execicios introdutorios do dia 4.4

---
+++ Módulo 1 - Fundamentos/Bloco 04: JavaScript e Lógica/4.4 JavaScript - Objetos e funções/Exercícios Introdutórios/Funções/Funções.js
@@ -0,0 +1,57 @@
+function soma (a,b) {
+  
+  let x = a;
+  let y = b;
+  let z = a + b;
+
+  return z;
+
+}
+
+function subtracao (a,b) {
+
+  let x = a;
+  let y = b;
+  let z = a - b;
+
+  return z;
+
+}
+
+function mutiplicaçao (a,b) {
+
+  let x = a;
+  let y = b;
+  let z = a * b;
+
+  return z;
+
+}
+
+function divisao (a,b) {
+
+  let x = a;
+  let y = b;
+  let z = a / b;
+
+  return z;
+
+}
+
+function modulo (a,b) {
+
+  let x = a;
+  let y = b;
+  let z = a % b;
+
+  return z;
+}
+
+const a = 9;
+const b = 7;
+
+console.log(soma(a,b));
+console.log(subtracao(a,b));
+console.log(mutiplicaçao(a,b));
+console.log(divisao(a,3));
+console.log(modulo(a,3));
