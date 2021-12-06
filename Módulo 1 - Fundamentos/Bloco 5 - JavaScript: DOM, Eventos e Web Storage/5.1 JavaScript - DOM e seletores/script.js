c3e1a76da9e402fd1c2146c2ce3a70d2b2608b22
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-06 20:03:58 -0300

    Exercicio introdutorio ao DOM, dia 5.1

---
+++ Módulo 1 - Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.1 JavaScript - DOM e seletores/script.js
@@ -0,0 +1,24 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "#a4243bff";
+
+let titulo = document.getElementById("page-title");
+titulo.textContent = "O Terminal";
+
+let segundoP = document.getElementById("second-paragraph");
+segundoP.textContent = "Aparentemente não sou muito criativo";
+segundoP.style.color = "#3fa7d6ff";
+
+let subtitulo = document.getElementById("subtitle");
+subtitulo.textContent = "O DOM é maravilhoso";
+
+let texto = document.getElementsByClassName("texto");
+console.log(texto);
+
+texto[0].innerHTML = "<b>Dê uma cor</b> para este <i>parágrafo</i>!";
+
+let tag = document.getElementsByTagName("p")[1];
+tag.style.backgroundColor = "#f79d84ff";
+tag.style.textShadow = "1px 1px 2px black";
+console.log(tag);
+
+// Recupere o subtítulo e altere a cor dele usando a função getElementsByTagName .
