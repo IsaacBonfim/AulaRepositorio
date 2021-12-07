a2efe30ede06c2c3f09127ddb256f381d7e68cf2
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-07 14:17:45 -0300

    Exercicio introdutorio do dia 5.2

--- Módulo 1 - Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 JavaScript - Trabalhando com elementos/Exercício Introdutório/script.js
+++ Módulo 1 - Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 JavaScript - Trabalhando com elementos/Exercício Introdutório/script.js
@@ -1,26 +1,27 @@
 // // Acesse o elemento elementoOndeVoceEsta.
+// // Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+// // Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+// // Acesse o primeiroFilho a partir de pai.
+// // Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+// // Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+// // Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+// // Agora acesse o terceiroFilho a partir de pai.
+
 let ondeEstou = document.getElementById('elementoOndeVoceEsta');
 console.log(ondeEstou);
 
-// // Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 ondeEstou.parentElement.style.backgroundColor = '#947bd3ff';
 console.log(ondeEstou.parentElement);
 
-// // Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
 ondeEstou.firstElementChild.textContent = 'É o carro do Ovo passando na sua rua';
 console.log(ondeEstou.firstElementChild);
 
-// // Acesse o primeiroFilho a partir de pai.
 console.log(ondeEstou.parentElement.firstElementChild);
 
-// // Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
 console.log(ondeEstou.previousElementSibling);
 
-// // Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
 console.log(ondeEstou.nextSibling);
 
-// // Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
 console.log(ondeEstou.nextElementSibling);
 
-// // Agora acesse o terceiroFilho a partir de pai.
 console.log(ondeEstou.parentElement.lastElementChild.previousElementSibling);
