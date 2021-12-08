9907a8d1936fd3a7fb096fd843c7aa2ada1bc385
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-07 23:04:05 -0300

    Exercicio de fixaçao do dia 5.2

--- Módulo 1 - Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 JavaScript - Trabalhando com elementos/Exercício de Fixação/script.js
+++ Módulo 1 - Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 JavaScript - Trabalhando com elementos/Exercício de Fixação/script.js
@@ -17,13 +17,31 @@ corpo.appendChild(principal);
 
 // ^ Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
 
+let centro = document.createElement('section');
+centro.className = 'center-content';
 
+principal.appendChild(centro);
 
-// Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+// ^ Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
 
-// Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+let paragrafo = document.createElement('p');
+paragrafo.textContent = 'Praticando inserção de elementos no HTML pot meio do JavaScript com DOM.'
 
-// Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+centro.appendChild(paragrafo);
+
+// ^ Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+
+let esquerda = document.createElement('section');
+esquerda.className = 'left-content';
+
+principal.appendChild(esquerda);
+
+// ^ Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+
+let direita = document.createElement('section');
+direita.className = 'right-content';
+
+principal.appendChild(direita);
 
 // Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
 
