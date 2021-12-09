6ef680510433a7223d07e2fc5da9b4e66e70f47d
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-09 03:39:03 -0300

    Exercicio introdutorio do dia 5.4

--- Módulo 1 - Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.4 JavaScript - Web Storage/Exercício de Fixação/script.js
+++ Módulo 1 - Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.4 JavaScript - Web Storage/Exercício de Fixação/script.js
@@ -1,13 +1,59 @@
+const body = document.getElementById('corpo');
+const section = document.getElementById('bgTexto');
+
+const fundoDaTela = document.getElementById('bg');
+const fundoDoTexto = document.getElementById('corBgTexto');
+const corDoTexto = document.getElementById('corDoTexto');
+const tamanhoDaFonte = document.getElementById('tamanhoFonte');
+const espacamento = document.getElementById('espacamento');
+const letras = document.getElementById('fonte');
+
 // ! As pessoas devem ter o poder de alterar:
 
-// * Cor de fundo da tela;
+// ^ Cor de fundo da tela;
 
-// * Cor do texto;
+function corDaTela(evento) {
+  body.style.backgroundColor = evento.target.value;
+}
 
-// * Tamanho da fonte;
+fundoDaTela.addEventListener('change', corDaTela);
 
-// * Espaçamento entre as linhas do texto;
+function corDoFundoDoTexto(evento) {
+  section.style.backgroundColor = evento.target.value;
+}
 
-// * Tipo da fonte ( Font family ).
+fundoDoTexto.addEventListener('change', corDoFundoDoTexto);
+
+// ^ Cor do texto;
+
+function corTexto(evento) {
+  section.style.color = evento.target.value;
+}
+
+corDoTexto.addEventListener('change', corTexto);
+
+// ^ Tamanho da fonte;
+
+function tamanho(evento) {
+  section.style.fontSize = evento.target.value + 'px';
+}
+
+tamanhoDaFonte.addEventListener('click', tamanho);
+
+// ^  Espaçamento entre as linhas do texto;
+
+function espacoLinhas(evento) {
+  section.style.lineHeight = evento.target.value + 'px';
+}
+
+espacamento.addEventListener('click', espacoLinhas);
+
+// ^  Tipo da fonte ( Font family ).
+
+function tipoDaLetra(evento) {
+  section.style.fontFamily = evento.target.value;
+}
+
+fonte.addEventListener('keyup', tipoDaLetra);
 
 // * Essas preferências devem ser salvas de forma que, ao retornar à página, as preferências que foram previamente configuradas possam ser aplicadas na tela.
