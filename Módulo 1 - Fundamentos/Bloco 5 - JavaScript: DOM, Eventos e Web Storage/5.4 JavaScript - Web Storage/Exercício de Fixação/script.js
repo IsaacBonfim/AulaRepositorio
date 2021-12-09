ecc15249d3b3d0f909faf25e59727a30ee10af8c
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2021-12-09 00:11:50 -0300

    Exercicio introdutorio do dia 5.4

--- Módulo 1 - Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.4 JavaScript - Web Storage/Exercício de Fixação/script.js
+++ Módulo 1 - Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.4 JavaScript - Web Storage/Exercício de Fixação/script.js
@@ -1,13 +1,13 @@
-// As pessoas devem ter o poder de alterar:
+// ! As pessoas devem ter o poder de alterar:
 
-// Cor de fundo da tela;
+// * Cor de fundo da tela;
 
-// Cor do texto;
+// * Cor do texto;
 
-// Tamanho da fonte;
+// * Tamanho da fonte;
 
-// Espaçamento entre as linhas do texto;
+// * Espaçamento entre as linhas do texto;
 
-// Tipo da fonte ( Font family ).
+// * Tipo da fonte ( Font family ).
 
-// Essas preferências devem ser salvas de forma que, ao retornar à página, as preferências que foram previamente configuradas possam ser aplicadas na tela.
+// * Essas preferências devem ser salvas de forma que, ao retornar à página, as preferências que foram previamente configuradas possam ser aplicadas na tela.
