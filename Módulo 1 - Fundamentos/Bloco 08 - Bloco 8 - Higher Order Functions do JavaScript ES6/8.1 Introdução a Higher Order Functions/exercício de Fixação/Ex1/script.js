39d9e07230a47422eeb287ccca12e00b99b55cf0
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-01-18 23:38:19 -0300

    Alteraçao no Scrip.js do exercicio 1 do dia 8.1

--- Módulo 1 - Fundamentos/Bloco 08 - Bloco 8 - Higher Order Functions do JavaScript ES6/8.1 Introdução a Higher Order Functions/exercício de Fixação/Ex1/script.js
+++ Módulo 1 - Fundamentos/Bloco 08 - Bloco 8 - Higher Order Functions do JavaScript ES6/8.1 Introdução a Higher Order Functions/exercício de Fixação/Ex1/script.js
@@ -8,9 +8,9 @@ const contratada = (string) => {
 
 const newEmployees = (action) => {
   const employees = {
-    id1: action('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: action('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: action('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: action('Pedro Guerra'), // Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: action('Luiza Drumond'), // Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: action('Carla Paiva'), // Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
