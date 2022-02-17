579a9f0a830b6d877a863f2362d67f4a2d6a4d4a
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-02-16 22:04:08 -0300

    Hello World em react

---
+++ Módulo 2 - Front-end/Bloco 10 - Introdução à React/10.1 Introdução a React/hello_world/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+import Component from './Component'
+
+function App() {
+  return <Component />;
+}
+
+export default App;
---
+++ Módulo 2 - Front-end/Bloco 10 - Introdução à React/10.1 Introdução a React/hello_world/src/Component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Isaac Oliveira</h1>
+        <p>Olá eu sou isaac e sou estudando da Trybe, moro em betim-MG</p>
+      </div>
+    )
+  }
+}
+
+export default Component;
